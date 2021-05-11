187f82a4728fdecb57b8bc07462340f94d21e865
Dmitriy <volga19171@gmail.com>
2021-05-11 17:01:16 +0400

    выполнено задание 2

--- src/main/java/MyFirstProgram.java
+++ src/main/java/MyFirstProgram.java
@@ -2,5 +2,10 @@ public class MyFirstProgram {
 
     public static void main(String[] args) {
         System.out.println("Hello world");
+
+        Point point1 = new Point(1, 1);
+        Point point2 = new Point(6, 1);
+
+        System.out.println(Point.distance(point1, point2));
     }
 }
---
+++ src/main/java/Point.java
@@ -0,0 +1,16 @@
+import static java.lang.Math.*;
+
+public class Point {
+
+    public double x;
+    public double y;
+
+    public Point(double x, double y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public static double distance(Point p1, Point p2) {
+        return sqrt((p2.x - p1.x) * (p2.x - p1.x) + (p2.y - p1.y) * (p2.y - p1.y));
+    }
+}
